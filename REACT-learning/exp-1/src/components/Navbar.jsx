4e4d79230765340529330ab12503fcb26074d139
khushi-gupta1910 <kg7825881@gmail.com>
2026-09-13 22:35:49 +0530

    New: component folder

---
+++ REACT-learning/exp-1/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <div>
+            Khushi Gupta
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
